9f76eaca9dbf867f57ef1915d1b84760d260c6a9
nknig <n.knight,pc0627@gmail.com>
2022-11-10 15:12:14 +0900

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        int i = 10000;
+        int c = 0;
+        while (true)
+        {
+            i /= 2;
+            c++;
+            if(i <= 100) break;
+        }
+        Console.WriteLine(c);
+    }
+}
